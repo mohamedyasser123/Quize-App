eb06b95f785d135ba7d7f705f5c9b8e9f86768ac
Malak994 <malakmohamedkamal1711@gmail.com>
2026-06-30 19:21:46 -0700

    forget done

---
+++ src/hooks/auth/useForgetPassword.ts
@@ -0,0 +1,50 @@
+"use client";
+
+import { forgetPasswordApi } from "@/src/services/auth";
+import { ForgetPasswordFormData } from "@/src/types/auth/forgetPassword-type";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+export default function useForgetPassword() {
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ForgetPasswordFormData>({
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = async (data: ForgetPasswordFormData) => {
+    try {
+      setIsSuccess(false);
+      setIsLoading(true);
+
+      await forgetPasswordApi(data);
+
+      setIsSuccess(true);
+
+      router.push("/resetPassword");
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    register,
+    handleSubmit,
+    errors,
+    isLoading,
+    isSuccess,
+    onSubmit,
+  };
+}
