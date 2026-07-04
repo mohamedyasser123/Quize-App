58420a65d63f6ac0ead881c28027247a4e655bc2
mohamedyasser123 <104241460+mohamedyasser123@users.noreply.github.com>
2026-07-04 19:17:20 +0300

    dashoard without quizes and isuss in student

---
+++ src/hooks/student/useStudent.ts
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { getStudentsApi } from "@/src/services/student";
+import toast from "react-hot-toast";
+import { Student } from "@/src/types/student";
+
+export default function useStudents() {
+  const [students, setStudents] = useState<Student[]>([]);
+  const [topStudents, setTopStudents] = useState<Student[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getStudents = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await getStudentsApi();
+
+      setStudents(response);
+
+      const topFive = [...response]
+        .sort((a, b) => b.avg_score - a.avg_score)
+        .slice(0, 5);
+
+      setTopStudents(topFive);
+    } catch {
+      toast.error("Failed to load students");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getStudents();
+  }, []);
+
+  return {
+    students,
+    topStudents,
+    isLoading,
+    refetch: getStudents,
+  };
+}
